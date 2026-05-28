7a917ca78535f417de831c2932be00b13b63bb6e
hiroko <webtailortokyo@gmail.com>
2026-05-28 17:02:53 +0900

    Add OGP image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://app.webtailor.work',
   vite: {
     plugins: [tailwindcss()]
   },
